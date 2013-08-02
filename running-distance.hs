2761071eae15b834857817691168527376675ada
Matt Gambogi <m@gambogi.com>
2013-08-02 00:45:20 -0400

    fixed single character snafu

--- running-distance.hs
+++ running-distance.hs
@@ -1,12 +1,16 @@
-module Running.Distance(
-          Foot
-        , Yard
-        , Mile
-        , Marathon
+module Running.Distance (
+          Distance(..)
+        , Foot
         , Meter
-        , Kilometer
+        , addDist
+        , subDist
+        , convertDist
         )
 where
+
+import Data.Data(Data)
+import Data.List(isPrefixOf)
+import Data.Typeable(Typeable)
 
 -- | A generic class that defines all the units of measure in running.
 -- The base unit of meters was chosen because Metric is orders more sane than
@@ -14,9 +18,9 @@ where
 
 class Distance d where
     -- | Converts the given unit of distance into meters
-    toMeters :: a -> Meter
+    toMeters :: d -> Float
     -- | Converts the given number of meters into the unit of measure
-    fromMeters :: Meters -> a
+    fromMeters :: Float -> d
 
 --------------------------------------------
 -- | Functions
@@ -30,5 +34,34 @@ subDist :: (Distance a, Distance b, Distance c) => a -> b -> c
 subDist x y = fromMeters (toMeters x + toMeters y)
 
 -- | Convert one distance to another.
-convertDistance :: (Distance a, Distance b) => a -> b
-convertDistance = fromMeters . toMeters
+convertDist :: (Distance a, Distance b) => a -> b
+convertDist = fromMeters . toMeters
+
+--------------------------------------------
+-- | Unit Definitions
+
+-- | English, Ascending
+
+--
+newtype Foot = Foot Float
+    deriving (Eq, Data, Num,Ord,Real,Typeable)
+
+instance Distance Foot where
+    toMeters (Foot x) = x*0.3048
+    fromMeters x      = Foot (x * 3.28084)
+
+instance Show Foot where
+    show (Foot x) = show x ++ "ft"
+
+
+-- | Metric Ascending
+
+--
+newtype Meter = Meter Float
+    deriving (Eq, Data, Num,Ord,Real,Typeable)
+
+instance Distance Meter where
+    toMeters (Meter x) = x
+    fromMeters x       = x
+instance Show Meter where
+    show (Meter x) = show x ++ "m"
