ff95cbf84950c69cb48ff8c51ecda06e3913dc6e
Matt Gambogi <m@gambogi.com>
2013-08-04 23:57:59 -0400

    finished ReadTD

--- src/RunningTools.hs
+++ src/RunningTools.hs
@@ -1,5 +1,17 @@
-module RunningPace (
+module RunningTools (
         )
     where
-
+import Data.Time.Clock
+import System.Time
+import System.Time.Utils
+import Data.String.Utils
 import RunningDistance
+
+readTD :: String -> DiffTime
+readTD t = secondsToDiffTime $ secs time
+        where
+         time = map read (split ":" t) :: [Integer]
+         secs [h,m,s] = h*3600 + m*60 + s
+         secs [m,s]   = m*60 + s
+         secs [s]      = s
+
